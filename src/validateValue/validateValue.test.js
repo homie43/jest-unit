cbc5077a4c320cae82df8e73cdf2c9aed2188cec
Vladimir <homie43@yandex.ru>
2023-11-01 19:12:37 +0300

    more tests

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -2,8 +2,23 @@ const validateValue = require('./validateValue');
 
 test('Валидация теста', () => {
     // название, а в коллбэке сама функция
-    expect(validateValue(150)) // в expect передаем резуьтат вычислений
+    expect(validateValue(99)) // в expect передаем резуьтат вычислений
         // после отработки expect можно вызвать методы для проверки резултата
         .toBe(true);
     // тест упадет, тк 150 больше 100, и мы не получим true
+});
+
+describe('validateValue', () => {
+    // если проверяем несколько сценариев, то оборачиваем все функции в describe
+    test('Корректное', () => {
+        expect(validateValue(10)).toBe(true);
+    });
+
+    test('Меньше корректного', () => {
+        expect(validateValue(-10)).toBe(false);
+    });
+
+    test('Больше корректного', () => {
+        expect(validateValue(1110)).toBe(false);
+    });
 });
